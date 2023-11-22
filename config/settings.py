ad38cb6b210dfe737d69e60e1ee771bae6317e5c
Benjamin <benjamin.cornejo.meneque@gmail.com>
2023-11-22 18:02:21 +0800

    fix

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=True)
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "web-production-9af4.up.railway.app"]
+ALLOWED_HOSTS = ["web-production-9af4.up.railway.app", "localhost", "127.0.0.1"]
 CSRF_TRUSTED_ORIGINS = ["https://web-production-9af4.up.railway.app"]
 
 # Application definition
