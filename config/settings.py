9bd4c626791dc06cd11cdc05e6fda81cd940e251
Benjamin <benjamin.cornejo.meneque@gmail.com>
2023-11-22 17:05:43 +0800

    add csrf origins

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-6hpsr7_uvd+dy)46%3*9w%r%%e=o%tdecgkk2&5=8j)dj$avht
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = ["https://web-production-0d23.up.railway.app/"]
 
 # Application definition
 
